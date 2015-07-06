5d8473d935de2ba8a1cc0db63a93fba736f52ac2
Toby Padilla <tobypadilla@gmail.com>
2015-07-06 10:56:53 -0700

    Initial Commit

---
+++ SpotifyMigrate/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SpotifyMigrate
+//
+//  Created by Toby Padilla on 7/6/15.
+//  Copyright © 2015 Toby Padilla. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
